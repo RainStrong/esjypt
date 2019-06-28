eeb324612117263bbf6ac99ccb7314db637629a0
RainStrong <46811767+RainStrong@users.noreply.github.com>
2019-06-28 10:46:01 +0800

    新增

--- src/main/java/com/esjypt/service/UserService.java
+++ src/main/java/com/esjypt/service/UserService.java
@@ -2,6 +2,7 @@ package com.esjypt.service;
 
 import com.esjypt.mapper.TbUserMapper;
 import com.esjypt.model.TbUser;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,9 +13,24 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class UserService {
+
+    private static final Logger logger = Logger.getLogger(UserService.class);
+
     @Autowired
     TbUserMapper tbUserMapper;
-    public TbUser selectByExample(int id){
-        return tbUserMapper.Sel(id);
+
+    public TbUser selectByExample(int id) {
+        String str = null;
+        try {
+            str.equals("");
+        } catch (Exception e) {
+
+            logger.error("获取列表失败，", e);
+            throw e;
+        }
+
+//        tbUserMapper.Sel(id);
+        return null;
+
     }
 }
